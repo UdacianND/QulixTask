61b6498ff54b6ffe2edef8ba85301aa1445a381f
Azizbek <ibnhamid777@gmail.com>
2022-08-20 17:51:22 +0500

    Exception

---
+++ Exceptions/ItemNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QulixTask.Exceptions
+{
+    public class ItemNotFoundException : ArgumentNullException
+    {
+        public ItemNotFoundException()
+        : base() { }
+        public ItemNotFoundException(string message)
+        : base(message) { }
+    }
+}
